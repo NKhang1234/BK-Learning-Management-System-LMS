6cc236f301146ce681e526fb50c6a68dc04f0182
NKhang1234 <nhatkhangpham130304@gmail.com>
2024-10-04 17:44:10 +0700

    Modify signup validation v1

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -1,14 +1,30 @@
 let users = JSON.parse(localStorage.getItem("users")) || [];
 class user {
   constructor() {}
-  validatePass(userpass) {
-    let value = userpass.length < 8 ? false : true;
-    return value;
+  validate(email, pass) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z0-9\d@$!%*?&]{8,}$/;
+    // Contain at least 1 number, 1 uppercase, 1 lowercase, 1 special digit and at least 8 digits.
+    if (!email.match(emailRegex)) {
+      alert("Wrong email format");
+      return false;
+    } else if (!pass.match(passRegex)) {
+      alert("The password must contain at least 1 number, 1 uppercase letter, 1 lowercase letter, 1 special character and at least 8 characters");
+      return false;
+    }
+    for(let i = 0; i< users.length;++i) {
+      if (users[i].email === email) {
+        alert("The email has been used by another user");
+        return false;
+      } else if (users[i].pass === pass) {
+        alert("The password has been used by another user");
+        return false;
+      }
+    }
+    return true;
   }
   signup(useremail, userpass, usertype) {
-    let validated = false;
-    validated = this.validatePass(userpass);
-    if (validated) {
+    if (this.validate(useremail, userpass)) {
       this.email = useremail;
       this.pass = userpass;
       this.usertype = usertype;
@@ -22,7 +38,7 @@ class user {
         window.location = "admin.html";
       }
     } else {
-      alert("Please fill correct details");
+      alert("Please fill the form again");
     }
   }
 }
